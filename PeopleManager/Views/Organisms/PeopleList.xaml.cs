e9b8c6da06f0a741b034c27dbd65f015acc0c2ee
Giovane Souza <developergiovanesouza@gmail.com>
2025-01-15 00:25:21 -0300

    fix: Dialog Visibility (Filtering)

--- PeopleManager/Views/Organisms/PeopleList.xaml.cs
+++ PeopleManager/Views/Organisms/PeopleList.xaml.cs
@@ -1,10 +1,8 @@
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using PeopleManager.Common;
 using PeopleManager.Events;
 using PeopleManager.Models;
 using PeopleManager.ViewModels;
-using PeopleManager.Views.Organisms.ControlPages;
 using Prism.Events;
 using System;
 using System.Linq;
@@ -16,6 +14,8 @@ namespace PeopleManager.Views.Organisms
     {
         private readonly PeopleListViewModel viewModel;
         private readonly ResourceLoader resourceLoader;
+        private bool filteredItems = false;
+
         public PeopleList()
         {
             this.InitializeComponent();
@@ -34,10 +34,12 @@ namespace PeopleManager.Views.Organisms
 
             var peopleList = viewModel.People;
 
-            if(string.IsNullOrEmpty(Name) && string.IsNullOrEmpty(SurName) && string.IsNullOrEmpty(CPF))
+            if ((string.IsNullOrEmpty(Name) && string.IsNullOrEmpty(SurName) && string.IsNullOrEmpty(CPF)) && !filteredItems)
             {
                 DialogTemplate.ShowDialog(this.XamlRoot, resourceLoader.GetString("EmptyFieldsDialogMessage"));
-            } 
+                return;
+            }
+
             if (!string.IsNullOrEmpty(Name) || !string.IsNullOrEmpty(SurName) || !string.IsNullOrEmpty(CPF))
             {
                 var filteredPerson = peopleList.Where(p =>
@@ -46,9 +48,16 @@ namespace PeopleManager.Views.Organisms
                 (string.IsNullOrWhiteSpace(CPF) || p.Cpf.StartsWith(CPF)));
 
                 if (filteredPerson != null)
-                    dataGridPeopleList.ItemsSource = filteredPerson;
+                {
+                    ListViewPeople.ItemsSource = filteredPerson;
+                }
+                filteredItems = true;
             }
-            else dataGridPeopleList.ItemsSource = viewModel.People;
+            else
+            {
+                filteredItems = false;
+                ListViewPeople.ItemsSource = viewModel.People;
+            }
         }
 
         private void SortPeopleBy(string sortBy)
@@ -56,74 +65,20 @@ namespace PeopleManager.Views.Organisms
             var allPeople = viewModel.People;
 
             if (sortBy == resourceLoader.GetString("PlaceholderName"))
-                dataGridPeopleList.ItemsSource = allPeople.OrderBy(p => p.Name).ToList();
+                ListViewPeople.ItemsSource = allPeople.OrderBy(p => p.Name).ToList();
             else if (sortBy == resourceLoader.GetString("PlaceholderLastName"))
-                dataGridPeopleList.ItemsSource = allPeople.OrderBy(p => p.Surname).ToList();
+                ListViewPeople.ItemsSource = allPeople.OrderBy(p => p.Surname).ToList();
             else
-                dataGridPeopleList.ItemsSource = allPeople.OrderBy(p => p.Id).ToList();
+                ListViewPeople.ItemsSource = allPeople.OrderBy(p => p.Id).ToList();
 
             /*
             _ = sortBy switch
             {
-                "Por Nome" => dataGridPeopleList.ItemsSource = allPeople.OrderBy(p => p.Name).ToList(),
-                "Por Sobrenome" => dataGridPeopleList.ItemsSource = allPeople.OrderBy(p => p.Surname).ToList(),
-                _ => dataGridPeopleList.ItemsSource = allPeople.OrderBy(p => p.Id).ToList()
+                "Por Nome" => ListViewPeople.ItemsSource = allPeople.OrderBy(p => p.Name).ToList(),
+                "Por Sobrenome" => ListViewPeople.ItemsSource = allPeople.OrderBy(p => p.Surname).ToList(),
+                _ => ListViewPeople.ItemsSource = allPeople.OrderBy(p => p.Id).ToList()
             };
             */
-        }
-
-        private async void DeletePerson_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-        {
-            if (sender is Button button && button.Tag is Person person)
-            {
-                var dialog = new ContentDialog
-                {
-                    XamlRoot = this.XamlRoot,
-                    Title = resourceLoader.GetString("DeleteDialogTitle"),
-                    Content = new TextBlock { Text = $"{person.Fullname} - {person.Cpf}" },
-                    PrimaryButtonText = resourceLoader.GetString("DeleteButton"),
-                    CloseButtonText = resourceLoader.GetString("CancelButton"),
-                    PrimaryButtonStyle = (Style)Application.Current.Resources["ButtonDeleteStyle"],
-                    CloseButtonStyle = (Style)Application.Current.Resources["ButtonCloseCancelStyle"],
-                    Style = (Style)Application.Current.Resources["DialogStyle"]
-                };
-
-                dialog.RequestedTheme = ThemeSettings.LoadUserPreferredTheme() == "Dark" ? ElementTheme.Dark : ElementTheme.Light;
-
-                var result = await dialog.ShowAsync();
-                
-                if(result == ContentDialogResult.Primary)
-                    EventAggregator.Current.GetEvent<DeletePersonEvent>().Publish(person.Id);
-            }
-        }
-
-        private async void EditPerson_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-        {
-            var person = sender is Button button ? button.Tag : null;
-            Person p = (Person)person;
-
-            EditPersonDialog editDialog = new()
-            {
-                XamlRoot = this.XamlRoot,
-                PersonName = p.Name,
-                PersonSurname = p.Surname,
-                PersonCpf = p.Cpf,
-                PrimaryButtonStyle = (Style)Application.Current.Resources["ButtonConfirmStyle"],
-                CloseButtonStyle = (Style)Application.Current.Resources["ButtonCloseCancelStyle"],
-                Style = (Style)Application.Current.Resources["DialogStyle"]
-            };
-
-            editDialog.RequestedTheme = ThemeSettings.LoadUserPreferredTheme() == "Dark" ? ElementTheme.Dark : ElementTheme.Light;
-
-            var result = await editDialog.ShowAsync();
-
-            if (result == ContentDialogResult.Primary)
-            {
-                Person editedPerson = new(p.Id, editDialog.PersonName, editDialog.PersonSurname, editDialog.PersonCpf);
-
-                if (p != editedPerson)
-                    EventAggregator.Current.GetEvent<UpdatePersonEvent>().Publish(editedPerson);
-            }
         }
     }
 }
